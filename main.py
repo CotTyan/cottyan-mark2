8c4514ab8a8344a8f45c90af097d1755fbd35be5
Laminne Tatsuto Yamamoto <gtattoda4@gmail.com>
2020-07-28 17:51:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,24 +24,28 @@ async def on_ready():
     
 @client.event
 async def on_member_join(member):
+    lrn = []
     for role in member.roles:
         lrn.append(role.name)
         if len(lrn) == 2:
             break
-    m = 'Hi!\nPlease read <#736628061289578496> and type "ok". \nIf you are going to to be able to do it, please introduse yourself in <#736627134604378143>'
+    m = "Hi!<@"+str(member.id)+">,\nPlease read <#736628061289578496> and type [ok]. \nIf you are going to to be able to do it, please introduse yourself in <#736627134604378143>\n"
     channel = client.get_channel(737576896476348447)
     await channel.send(m)
-
-     #channel = client.get_channel(payload.channel_id)
-     #   if channel.id != ID_CHANNEL_README:
-     #       return    
+    
 
 @client.event
 async def on_message(message):
     if message.guild.id == server_id:
         if message.content == "ok":
+            lrn = []
+            for role in message.author.roles:
+                lrn.append(role.name)
+                if len(lrn) == 2:
+                    return
             role = discord.utils.get(message.guild.roles, name="自己紹介してね")
             return await message.author.add_roles(role)
 
 if __name__ == "__main__":
     client.run(TOKEN)
+    
