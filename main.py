b75aeb1b232a03deac19cde9587291647471fe21
Laminne Tatsuto Yamamoto <gtattoda4@gmail.com>
2020-07-28 15:33:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
+import asyncio
 import discord
-
-client = discord.Client()
 TOKEN = ""
+entry_id = 667385969716232194
+server_id = 665189315877535753
+hello_id =
+
+server_id = int(server_id)
+entry_id = int(entry_id)
+hello_id = str(hello_id)
+client = discord.Client()
+
+async def send(channel,*args, **kwargs): return await channel.send(*args, **kwargs)
+
 @client.event
-async def on_member_join(member):
-     m = "ようこそ<@"+str(member.id)+">さん\n<#667385969716232194>を読んでから「ok」と入力してください\nそのあと<#736627134604378143>で自己紹介をしていただければ書き込み可能となります"
-     return await send(client.get_channel("667385969716232194"), m)
+async def on_ready():
+    print(client.user.id)
+    print('It is ready')
+    channel = client.get_channel(hello_id)
+    embed = discord.Embed(title="ただ今起動しました",description="動作していない場合は管理者に問い合わせてください")
+    embed = embed.set_author(name="CotTyanからのお知らせ",icon_url="https://www.repo.approvers.dev/g2058.png)
+    await channel.send(embed=embed)
     
+
+async def on_member_join(member):
+     m = 'Hi!\n Please read<#667385969716232194>and type "ok". \n If you are going to to be able to do it, please introduse yourself in<#736627134604378143>.'
+     return await member.send(m)
+
+     #channel = client.get_channel(payload.channel_id)
+     #   if channel.id != ID_CHANNEL_README:
+     #       return    
 
 @client.event
 async def on_message(message):
-    if message.guild.id == "665189315877535753":
-        if message.content.lower() == "ok":
-            role = discord.utils.get(message.guild.roles, name="自己紹介してね")
+    if message.guild.id == server_id:
+        if message.content == "ok":
+            role = discord.utils.get(message.guild.roles, name="user")
             return await message.author.add_roles(role)
 
 if __name__ == "__main__":
