c0c01ccd4d697feb6a9eac7aeae744e6a127676b
Laminne Tatsuto Yamamoto <gtattoda4@gmail.com>
2020-08-02 12:08:15 +0900

    Update searchkey.py

--- searchkey.py
+++ searchkey.py
@@ -2,20 +2,35 @@ import discord
 import os
 import asyncio
 import requests
+from urllib.parse import urljoin
+
 client = discord.Client()
+
 
 @client.event
 async def on_message(message):
-if massage.author.bot
-    return
-if message.content == "*gpg"
-    await message.channel.send('GitHubに登録されているユーザーのGPG公開鍵を取得します ユーザー名を続けて入力して下さい')
-    def check(command):
-        return command.author == message.author
-    c = await client.wailt_for('message', check=check)
+    if message.author.bot:
+        return
+    if message.content == "*gpg":
+        await message.channel.send("GitHubに登録されているユーザーのGPG公開鍵を取得します ユーザー名を続けて入力して下さい")
+
+        def check(command):
+            return command.author == message.author
+
+        c = await client.wait_for("message", check=check)
+    # c = await client.wait_for("message")
 
     search = c.content
     count = 0
+    print(search)
+    search = str(search)
     baseurl = "https://github.com/"
-    res = requests.get(url+c)
-    await message.channel.send(res)
+    # username = search + ".gpg"
+    url = urljoin("https://github.com/", search)
+    url = url + ".gpg"
+    print(url)
+    res = requests.get(url)
+    await message.channel.send(f"```{res.text}```")
+
+
+client.run("")
